ba0b76825ce3949d02ec0dc9347154ae8b387963
Hubert <hubert.kosiorowski1@gmail.com>
2025-08-21 13:56:10 +0200

    poisson_sampling.py done

--- src/data_generation/picture_generator.py
+++ src/data_generation/picture_generator.py
@@ -1,6 +1,4 @@
 import cv2
 import numpy as np
 
-def generate_picture(width = 500, height = 500):
-
-    image = np.zeros((width, height, 3 ), np.uint8)
+# def generate_picture(width = 500, height = 500):
